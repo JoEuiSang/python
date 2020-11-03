6870914deab3f8341ee6e4415b293b91198da7e7
JES <ourjes@naver.com>
2020-11-03 15:18:56 +0900

    2일차 for문 연습

---
+++ 1주차/11월3일/반복문/for문.py
@@ -0,0 +1,19 @@
+num = [1, 2, 123, 4, 5, 6, 7, 8, 9]
+
+sum=0
+
+# i에 num의 요소들이 하나씩 매칭댐
+for i in num:
+    sum+=i
+    print(i)
+
+print("총합: ", sum)
+
+
+"""
+s = 'asdfasdfasdf'
+
+i=0
+while i< len(s):
+    print(s[i])
+    i+=1"""
