2cef17ddbe570180834f28261ba4b56c3da76409
JES <ourjes@naver.com>
2020-11-06 17:44:48 +0900

    5일차 함수의다형성

---
+++ 1주차/11월6일/함수의다형성.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    return a+b
+
+def main():
+    print(add(1,2))
+    print(add('aaa','bbb'))
+    print(add(1.23,4.56))
+    print(add([1,2],[3,4,5]))
+    # print(add(1,'asdf'))#error
+
+main()
+
